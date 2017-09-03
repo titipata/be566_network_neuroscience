999658a8c2e6d5481e7952d41340b96b8ffd14bd
titipata <my.titipat@gmail.com>
2017-09-03 18:12:38 -0400

    Initial code for project 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# BE566 Network Neuroscience Projects
+
+Repository for BE566 projects. This is network class taught by [Prof. Danielle Bassett](http://www.danisbassett.com/)
+at University of Pennsylvania.
---
+++ project1/utils.py
@@ -0,0 +1,159 @@
+import re
+import dedupe
+from unidecode import unidecode
+from bs4 import BeautifulSoup
+import pandas as pd
+from itertools import chain, combinations
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def parse_text_with_sup(text):
+    """
+    Parse given affiliation text to list of author and affiliation
+    """
+    authors = text.split('; <br/>')[0]
+    affiliation = text.split('; <br/>')[1]
+    affiliations = [a for a in affiliation.split('<sup>') if a.strip() is not '']
+    affil_dict = {}
+    for a in affiliations:
+        sup, affil =  a.split('</sup>')
+        affil_dict[sup] = affil
+
+    author_affil_list = []
+    for a in authors.split(', '):
+        soup = BeautifulSoup(a, 'html.parser')
+        try:
+            sup = soup.find('sup').text
+        except:
+            sup = ''
+        affil_author = affil_dict.get(sup, '')
+        author = soup.text
+        author = re.sub(sup, '', author).replace(',', '')
+        author = re.sub('[\d+]', '', author)
+        author_affil_list.append((author, affil_author))
+    return author_affil_list
+
+
+def parse_sfn_author(text):
+    try:
+        authors, affliation = text.split('; <br/>')
+        replace_dict = {', III': ' III', ', IV': ' IV', ', II': ' II'}
+        for (r1, r2) in replace_dict.items():
+            authors = authors.replace(r1, r2)
+        authors = BeautifulSoup(authors, 'html.parser').text
+        if '<sup>' in text:
+            return parse_text_with_sup(text)
+        elif ',' not in authors:
+            authors = authors
+            affiliation = BeautifulSoup(affliation, 'html.parser').text
+            return [(authors, affiliation)]
+        elif ',' in authors:
+            authors = authors.split(', ')
+            affiliation = BeautifulSoup(affliation, 'html.parser').text
+            return [(a, affiliation) for a in authors]
+        else:
+            return []
+    except:
+        return []
+
+
+def apply_dedupe(authors):
+    """
+    Give list of authors and publication id as
+        [(presentation_id, name, affiliation), ...]
+    Tag using dedupe library and return
+    """
+    fields = [{'field': 'Name', 'type': 'String', 'has missing': True},
+              {'field': 'Affiliation', 'type': 'String', 'has missing': True}]
+    deduper = dedupe.Dedupe(fields)
+
+    # prepare dedupe dictionary
+    author_dict = {}
+    for i, author in enumerate(authors):
+        if author[1] != '' and author[2] != '':
+            author_dict[i] = {'PaperId': author[0],
+                              'Name': unidecode(author[1] or '').replace('-.', ' ').replace('.', '').replace('-', ' ').upper(),
+                              'Affiliation': unidecode(author[2] or '').replace(',', '').replace('.', '').lower()}
+    deduper.sample(author_dict, 1000)
+    dedupe.consoleLabel(deduper)
+    deduper.train()
+    threshold = deduper.threshold(author_dict, recall_weight=1.0)
+    clustered_dupes = deduper.match(author_dict, threshold)
+    clustered_dupes_pair = [pair for pair, _ in clustered_dupes] # pairs
+
+    author_ids = []
+    for i, pair in enumerate(clustered_dupes_pair):
+        author_ids.extend([(i, e) for e in pair])
+    authors_unique = list(set(range(len(authors))) - set([author_id for _, author_id in author_ids]))
+
+    aid = author_ids[-1][0]
+    for author in authors_unique:
+        aid = aid + 1
+        author_ids.append({'author_id': aid, 'author_number': author})
+    author_id_df = pd.DataFrame(author_ids)
+    return author_id_df
+
+def create_collaboration_graph(author_df):
+    """
+    Create graph where each nodes is unique author and each edges is collaboration
+    """
+    nodes_df = author_df[['affiliation', 'author_name', 'author_id']].drop_duplicates(subset='author_id')
+    G = nx.Graph()
+    for _, row in nodes_df.iterrows():
+        G.add_node(row['author_id'], attr_dict={'affiliation': row['affiliation'],
+                                                'author_name': row['author_name']})
+    # get papers with more than 2 authors
+    n_papers_df = author_df[['paper_id']].groupby('paper_id').size().reset_index()
+    n_papers_df.columns = ['paper_id', 'n_authors']
+    n_papers_df = n_papers_df[n_papers_df.n_authors > 1]
+
+    # add edges
+    edges = []
+    for paper_id in n_papers_df.paper_id:
+        authod_ids = list(author_df[author_df.paper_id == paper_id].author_id)
+        edges.extend(list(combinations(authod_ids, 2)))
+    for n1, n2 in edges:
+        G.add_edge(n1, n2)
+    return G
+
+
+def hist_publication(df):
+    """
+    Using Altair to plot histogram of chart
+    """
+    chart = Chart(df).mark_bar().encode(
+        color=Color(value='#fbb33a'),
+        x=X('n_authors:Q', bin=Bin(maxbins=30), title='Number of authors in a poster'),
+        y=Y('count(*):Q', title='Number of posters'),
+    ).configure_facet_cell(
+        strokeWidth=0.0,
+    )
+    return chart
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('SfN2017_RecommendationEngine.csv')
+    df.columns = ['presentation_id', 'presentation_number', 'start_time', 'title', 'abstract', 'author', 'location']
+    df.fillna('', inplace=True)
+    df['parsed_authors'] = df.author.map(parse_sfn_author)
+
+    authors = list()
+    for i, row in df.iterrows():
+        for a in row['parsed_authors']:
+            if len(row['parsed_authors']) > 0]) and a[1] is not '':
+                authors.extend((row['presentation_id'], a[0].replace('*', ''), a[1]))
+
+    # applying deduplication algorithm
+    author_id_df = apply_dedupe(authors)
+
+    author_number_list = []
+    for i, (paper_id, author_name, affiliation) in enumerate(authors):
+        author_number_list.append({'author_number': i,
+                                   'paper_id': paper_id,
+                                   'author_name': author_name,
+                                   'affiliation': affiliation})
+    author_number_df = pd.DataFrame(author_number_list)
+    author_df = author_number_df.merge(author_id_df, how='left') # final author number
+    author_df.to_csv('author_id.csv', index=False)
