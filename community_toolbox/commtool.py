ffbdb6530a087ae0c210e4bcde387f2f98768fda
titipata <my.titipat@gmail.com>
2017-10-01 22:55:00 -0400

    Update functions

--- community_toolbox/commtool.py
+++ community_toolbox/commtool.py
@@ -1,4 +1,5 @@
 import numpy as np
+import networkx as nx
 from collections import Counter
 from scipy.spatial import ConvexHull
 from sklearn.metrics.pairwise import pairwise_distances
@@ -523,6 +524,121 @@ def consensus_iterative(C):
     Q2: associated modularity value
     X_new3: thresholded nodal association matrix
     qpc: quality of the consensus (lower == better)
+
+    Reference:
+    http://commdetect.weebly.com/uploads/4/9/5/9/49593677/consensus_iterative.m
+    """
+
+    return
+    
+
+def zrand(part1, part2):
+    """
+    Calculates the z-Rand score and Variation of Information
+    distance between a pair of partitions.
+
+    Calculates the z-score of the
+    Rand similarity coefficient between partitions part1 and part2. The
+    Rand similarity coefficient is an index of the similarity between the
+    partitions, corresponding to the fraction of node pairs identified the
+    same way by both partitions (either together in both or separate in
+    both)
+
+    Inputs
+    ======
+    part1, Partitions that are being
+    part2, compared with one another
+
+    Outputs
+    =======
+    zRand, z-score of the Rand similarity coefficient
+    SR, Rand similarity coefficient
+    SAR, Adjusted Rand similarity coefficient
+    VI, Variation of information
+
+    Reference:
+    Amanda L. Traud, Eric D. Kelsic, Peter J. Mucha, and Mason A. Porter,
+    "Comparing Community Structure to Characteristics in Online Collegiate
+    Social Networks," SIAM Review 53, 526-543 (2011).
+
+    https://github.com/nangongwubu/Python-Version-for-Network-Community-Architecture-Toobox/blob/master/ncat.py#L14-L128
+    """
+
+    part1 = np.array(part1).ravel()
+    part2 = np.array(part2).ravel()
+    n = len(part1)
+    if part1.shape != part2.shape:
+        print('ERROR: partitions not of equal length')
+        return
+    n_1 = len(set(part1)) + 1
+    n_2 = len(set(part2)) + 1
+    nij = sparse.csr_matrix((np.ones(part1.shape, dtype=int), (part1, part2)),
+                             shape=(n_1, n_2))
+    ni = nij.sum(axis=1)
+    nj = nij.sum(axis=0)
+    nj = nj.T
+
+    M = n * (n - 1)/2.
+    M1 = ((ni.multiply(ni) - ni)) / 2.).sum()
+    M2 = ((nj.multiply(nj) - nj)) / 2.).sum()
+
+
+    a = ((nij.multiply(nij) - nij) / 2.).sum()
+    b = M1 - a
+    c = M2 - a
+    d = M - (a + b + c)
+    SR=(a + d) / (a + b + c + d)
+    meana = M1 * M2 / M
+    SAR = (a - meana) / ((M1 + M2)/2. - meana)
+
+    C1 = 4*((np.power(ni, 3)).sum()) - 8 * (n+1) * M1 + n * (n * n - 3 * n - 2)
+    C2 = 4*((np.power(nj, 3)).sum()) - 8 * (n+1) * M2 + n * (n * n - 3 * n - 2)
+
+    vara = M/16 - np.power((4 * M1 - 2 * M), 2) * np.power((4 * M2 - 2 * M), 2)/(256 * M * M) + \
+        C1*C2/(16*n*(n-1)*(n-2)) + \
+        (np.power((4 * M1 - 2 * M), 2) - 4 * C1 - 4 * M) * (np.power((4*M2-2*M),2) - 4 * C2 - 4 * M)/(64 * n * (n-1) * (n-2)*(n-3))
+
+    zRand = (a - meana) / np.sqrt(vara)
+
+    c1 = set(part1);
+    c2 = set(part2);
+    H1, H2, I =0, 0, 0
+    for i in c1:
+        pi = np.double(ni[i])/n
+        H1 = H1 - pi * np.log(pi)
+        for j in c2:
+            if nij[i,j]:
+                pj = np.double(nj[j])/n;
+                pij = np.double(nij[i,j])/n;
+                I = I + pij * np.log(pij/ (pi * pj))
+    for j in c2:
+        pj = np.double(nj[j]) / n
+        H2 = H2 - pj * np.log(pj)
+    VI = (H1 + H2 - 2 * I)
+    return zRand, SR, SAR, VI
+
+
+def consensus_similarity(C):
+    """
+    Construct a consensus (representative) partition
+    using the iterative thresholding procedure
+
+    identifies a single representative partition from a set of C partitions
+    that is the most similar to the all others. Here, similarity is taken
+    to be the z-score of the Rand coefficient (see zrand.m)
+
+
+    Inputs
+    ======
+    C, pxn matrix of community assignments where p is the
+        number of optimizations and n the number of nodes
+
+    Outputs
+    =======
+    consensus, consensus partition
+    consensus_simm,	average similarity between consensus
+        partition and all others
+    pairwise_simm,	pairwise similarity matrix
     """
 
     return
@@ -563,6 +679,9 @@ def community_louvain(W, gamma=1, ci=None, B='modularity', seed=None):
         final community structure
     q : float
         optimized q-statistic (modularity only)
+
+    Reference:
+    https://github.com/aestrivex/bctpy/blob/master/bct/algorithms/modularity.py#L66-L233
     '''
     np.random.seed(seed)
 
@@ -723,3 +842,311 @@ def multislice_static_unsigned(A, g_plus=1):
     Q = Q/sum(Aplus)
 
     return S, Q, lAlambda
+
+
+def symm_matrix(A, binary=False):
+    """
+    A function to symmetrize an input matrix. The procedure
+    by which items are symmetrized such that:
+    in the binary case:
+        if a(i,j) || a(j,i) == 1 for i,j in A
+            w(i,j) && w(j,i) == 1
+    in  the weighted case:
+        if (a(i,j) || a(j,i) > 0 for i,j in A
+            w(i,j) && w(j,i) == (a(i,j) + a(j,i) )/ 2
+
+    Inputs
+    ======
+    A: np.array, the binary or weighted input matrix
+    binary: bool, the key to indicate whether weighted or binary analysis
+        will take place
+        True indicates binarized, False indicates weighted
+
+    Outputs
+    =======
+    W: The symmeterized matrix
+
+    if binary analysis is specified, let binary symmeterization take place
+
+    written by Eric W. Bridgeford
+    """
+    if binary:
+        W = A.astype(int)
+        for (i, j) in combinations(range(len(A)), 2):
+            if A[i, j] or A[j, i]:
+                W[i, j] = 1
+                W[j, i] = 1
+    else:
+        W = (A + A.T) / 2.0
+
+    return W
+
+
+def regular_matrix_generator(G, r):
+    """
+    Generates a regular matrix, with weights obtained form the
+    original adjacency matrix representation of the network
+
+    note that all inputs should be symmeterized prior to forming a regular
+    matrix, since otherwise half of the connnections will be trashed. This
+    can be accomplished with the built in symm_matrix function, however,
+    the function is not performed here so that users can use their own
+    symmeterization procedure.
+
+    Inputs
+    ======
+    G: the adjacency matrix for the given network; must be symmmeterized
+    r: the approximate radius of the regular network
+
+    Outputs
+    =======
+    M: the regular matrix for the given network, where all
+        weights are sorted such that the inner radius has the
+        highest weights randomly distributed across the nodes,
+        and so on
+    """
+    n = len(G)
+    r = int(r)
+    B = np.triu(G).ravel()
+    B = np.sort(B, axis=0)
+    B = np.flip(B, axis=0)
+    n_elems = np.ceil(len(G.ravel())/ (2*len(G)))
+    n_zeros = 2 * n * n_elems - len(G.ravel())
+    B = np.concatenate((B, np.zeros(n_zeros.astype(int))))
+    B = np.reshape(B, (n, int(len(B)/n)), order='F')
+
+    M = np.zeros((n, n))
+    for i in range(n):
+        for z in range(r):
+            a = np.random.randint(0, n)
+            while B[a, z] == 0 and z != r:
+                a = np.random.randint(0, n)
+            y_coor = np.mod(i + z - 1, n) + 1
+            M[i, y_coor] = B[a, z]
+            M[y_coor, i] = B[a, z]
+            B[a, z] = 0
+    return M
+
+
+def randomize_matrix(A):
+    """
+    This code creates a random undirected network from the connectivity
+    distribution of an undirected adjacency matrix, ie, the intital matrix
+    must be symmetric.
+
+    Inputs
+    ======
+    A: an undirected adjacency matrix (symmetric) with no self connections
+
+    Outputs
+    =======
+    A_rand: a comparable random network with same number of nodes and
+        connectivity distribution
+
+    written by Sarah F. Muldoon
+    """
+    n_nodes = len(A)
+    A_rand = np.zeros((n_nodes, n_nodes))
+
+    orig_edges = A[np.triu_indices(len(A), k=1)]
+    n_edges = len(orig_edges)
+
+    randomized_edges = np.random.permutation(orig_edges)
+    edge = 0
+    for i in range(n_nodes):
+        for j in range(i + 1, n_nodes):
+            A_rand[i, j] = randomized_edges[edge]
+            A_rand[j, i] = randomized_edges[edge]
+            edge = edge + 1
+    return A_rand
+
+
+def clustering_coef_matrix(W, method='O'):
+    """
+    A modification of the clustering coefficient function provided
+    in the brain connectivity toolbox
+
+    improved definition of Onnela Clustering Coefficient, as well as
+    implementation of function for Zhang and Barrat clustering values
+
+    Reference:
+    - Onnela et al., Phys. Rev. E71, 065103(R)(2005)
+    - B.Zhang and S. Horvath, Stat. App. Genet. Mol. Biol.4, 17(2005)
+    - Barrat et al., Proc. Natl. Acad. Sci. U.S.A.101, 3747(2004)
+    - Watts and Strogatz (1998) Nature 393:440-442
+
+    Inputs
+    ======
+    W: the weighted or unweighted connectivity matrix
+    method: str, to represent the method to be used for computing
+        the clustering coefficient
+        possible strings: 'O' (Onnela), 'Z' (Zhang), 'B' (Barrat), 'bin' (binary)
+
+    """
+
+    if method == 'O':
+        K = np.sum(W, axis=1).astype(float)
+        W2 = W / W.max()
+        cyc3 = np.diag((W2 ** 1/3) ** 3)
+        K[cyc3 == 0] = np.inf
+        C = cyc3 / (K * (K - 1))
+        return C
+
+    elif method == 'bin':
+        G = (W > 0).astype(float)
+        n = len(G)
+        C = []
+        for u in range(n):
+            V = np.where(G[u, :])[0]
+            k = len(V)
+            if k >= 2:
+                S = G[V[:, None], V]
+                C.append(S.sum() / (k**2 / k))
+        return np.array(C)
+
+    elif method == 'Z':
+        C = []
+        n = len(W)
+        K = np.sum(W, axis=1)
+        W = W.astype(float)
+        W2 = W / W.max()
+        cyc3 = np.diag(W2 ** 3)
+        for i in range(n):
+            denom = np.sum(W2[i, :]) ** 2 - np.sum(W2[i, :] ** 2)
+            C.append(denom)
+        return np.array(C)
+
+    elif method == 'B':
+        C = []
+        n = len(G)
+        G = (W > 0).astype(float)
+        for i in range(n):
+            sum1 = 0
+            for j in range(n):
+                for k in range(n):
+                    sum1 = sum1 + ((W[i, j] + W[i, k]) / 2.) * A[i, j] * A[j, k] * A[i, k]
+            c_i = sum1 / (np.sum(W[i, :]) * np.sum(G[i, :]))
+            C.append(c_i)
+        return np.array(C)
+
+    else:
+        print("Select methods from 'O', 'bin', 'Z' or 'B'")
+
+
+def avg_clus_matrix(M, method='O'):
+    """
+    A function to compute the average clusteirng coefficient for a
+    input matrix M
+
+    Inputs
+    ======
+    W: a matrix, weighted or unweighted
+    method: a string, to represent the method to be used for computing
+        the clustering coefficient
+        possible strings: 'O' (Onnela), 'Z' (Zhang), 'B' (Barrat), 'bin' (binary)
+
+    Outputs
+    =======
+    Clus  the average clustering coefficient
+    """
+    C = clustering_coef_matrix(M, method)
+    return np.nanmean(C)
+
+
+def avg_path_matrix(M):
+    """
+    A function to compute the average path length of a given matrix
+    using the graphallshortestpaths built-in matlab function
+
+    written by Eric Bridgeford
+    """
+    M = np.array(M)
+    r, c = np.tril_indices(len(M))
+    G = nx.Graph()
+    n = len(M)
+    for r_, c_ in zip(r, c):
+        if not np.isinf(M[r_, c_]):
+            G.add_edge(r_, c_, weight=M[r_, c_])
+    return nx.average_shortest_path_length(G)
+
+
+def small_world_propensity(A, method='O'):
+    """
+    A function for calculating the small world propensity of
+    a given network - assumes that matrix is undirected (symmeteric) and if
+    not, creates a symmetric matrix which is used for the calculations
+
+    Inputs
+    ======
+    A: np.array, the connectivity matrix, weighted or binary
+    method: str, a string corresponding to the method of clustering
+        to be used, where 'O' is Onnela, 'Z' is Zhang,
+        'B' is Barrat, 'bin' is binary (default is Onnela).
+        If the user specifies binary analysis, a
+        weighted matrix will be converted to a binary matrix
+        before proceeding.
+
+    Outputs
+    =======
+    SWP: the small world propensity of the matrix
+    delta_C: the fractional deviation from the expected culstering coefficient of a
+        random network
+    delta_L: the fractional deviation from the expected path length of a
+        random network
+
+    written by Eric Bridgeford and modified by Sarah F. Muldoon
+
+    Reference:
+        Muldoon, Bridgeford, and Bassett (2015) "Small-World Propensity in Weighted,
+        Real-World Networks" http://arxiv.org/abs/1505.02194
+    """
+    if A.sum() > 0:
+        bin_matrix = False
+        if method == 'bin':
+            bin_matrix = True
+            A = (A > 0).astype(int)
+        sym_check = np.abs(A - A.T).sum()
+        if sym_check > 0:
+            W = symm_matrix(A, False)
+        else:
+            W = A
+        n = len(W)
+        dens_net = W.sum()/(W.max() * n * (n - 1))
+        n_connections = (W > 0).sum()
+        avg_deg_unw = n_connections / n
+        avg_rad_unw = avg_deg_unw / 2.
+        avg_rad_eff = np.ceil(avg_rad_unw)
+
+        # compute the average degree of the unweighted network, to give
+        # the approximate radius
+        W_reg = regular_matrix_generator(W, avg_rad_eff)
+        W_rand = randomize_matrix(W)
+
+        # compute all path length calculations for the network
+        reg_path = avg_path_matrix(1 / W_reg) # path of the regular network
+        rand_path = avg_path_matrix(1 / W_rand) # path of the random netowork
+        net_path = avg_path_matrix(1 / W) # path of the network
+
+        A = net_path - rand_path
+        if A < 0:
+            A = 0
+        diff_path = A/ (reg_path - rand_path)
+
+        reg_clus = avg_clus_matrix(W_reg, method)
+        rand_clus = avg_clus_matrix(W_rand, method)
+        net_clus = avg_clus_matrix(W, method)
+
+        B = (reg_clus - net_clus)
+        if B < 0:
+            B = 0
+        diff_clus = B / (reg_clus - rand_clus)
+        if np.isnan(reg_clus) or np.isnan(rand_clus) or np.isnan(net_clus):
+            diff_clus = 1
+        if diff_clus > 1:
+            diff_clus = 1
+
+        SWP = 1 - (np.sqrt(diff_clus ** 2 + diff_path ** 2)/np.sqrt(2))
+        delta_C = diff_clus
+        delta_L = diff_path
+
+        return SWP, delta_C, delta_L
