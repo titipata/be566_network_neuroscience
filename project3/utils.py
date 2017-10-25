302072268ac01b9b84672787de203afd4f5832dd
titipata <my.titipat@gmail.com>
2017-10-24 22:55:55 -0400

    Fix the code, according to Martin's comment

--- project3/utils.py
+++ project3/utils.py
@@ -7,6 +7,7 @@ from scipy.spatial.distance import cdist, pdist
 from sklearn.metrics.pairwise import pairwise_distances
 import fastcluster
 import hcluster
+from sklearn.preprocessing import normalize
 
 
 def calculate_entropy(p, alpha):
@@ -16,40 +17,72 @@ def calculate_entropy(p, alpha):
     """
     if not sp.issparse(p):
         p = sp.csr_matrix(p.ravel())
+
+    if alpha == 0:
+        H = p.shape[1] - 1
+    elif alpha == 1:
+        H = - np.sum(p.data * np.log(p.data))
+    elif alpha == 2:
+        H = 1 - (p.data ** 2).sum()
     else:
-        if alpha == 1:
-            H = - np.sum(p.data * np.log(p.data))
-        elif alpha == 2:
-            H = 1 - (p.data ** 2).sum()
-        else:
-            H = ((p.data ** alpha).sum() - 1)/ (1 - alpha)
+        H = ((p.data ** alpha).sum() - 1)/ (1 - alpha)
     return H
 
 
-def distance_language(p1, p2, alpha=2):
+def calculate_max_entropy(h1, h2, pi1=0.5, pi2=0.5, alpha=2):
+    """
+    Maximum entropy
+
+    h1 : entropy of probability distribution p1
+    h2 : entropy of probability distribution p2
+    """
+    if alpha == 1:
+        d_max = - pi1  * np.log(pi1) - pi2 * np.log(pi2)
+    else:
+        d_max = (pi1 ** alpha - pi1) * h1 + \
+            (pi2 ** alpha - pi2) * h2 + \
+            (pi1 ** alpha + pi2 ** alpha - 1)/(1 - alpha)
+    return d_max
+
+
+def distance_language(p1, p2, alpha=2, norm=True):
     """
     Distance between two articles using Jensen-Shannon Divergence
     with given alpha
     """
     h1 = calculate_entropy(p1, alpha)
     h2 = calculate_entropy(p2, alpha)
-    h12 = calculate_entropy((p1 + p2) / 2, alpha)
-    d_lang = ((2 * h12) - h1 - h2)/ (0.5 * (2 - h1 - h2))
+    h12 = calculate_entropy(0.5 * p1 + 0.5 * p2, alpha=alpha)
+    d_lang = h12 - 0.5 * h1 - 0.5 * h2
+    if norm:
+        d_max = calculate_max_entropy(h1, h2, pi1=0.5, pi2=0.5, alpha=alpha)
+        d_lang = d_lang / d_max
     return d_lang
 
 
-def calculate_entropy_dist(abstracts, sparse=True):
+def calculate_entropy_dist(texts, max_df=0.8, min_df=4,
+                           stop_words='english',
+                           max_features=20000,
+                           lowercase=True,
+                           metric='jensen-shannon'):
     """
-    Calculate entropy distance matrix between documents
+    Calculate entropy distance matrix between texts in the fields
+
+    texts: list of texts each contains all text in the fields
+    alpha: int, default to 2, you can change default value in distance_language function
+
     """
-    count_vec_model = CountVectorizer(max_df=0.8, min_df=4,
-                                      stop_words='english',
-                                      lowercase=True)
-    X = count_vec_model.fit_transform(abstracts)
+    count_vec_model = CountVectorizer(max_df=max_df, min_df=min_df,
+                                      stop_words=stop_words,
+                                      max_features=max_features,
+                                      lowercase=lowercase)
+    X = count_vec_model.fit_transform(texts)
     n_samples, n_features = X.shape
-    gf = np.ravel(X.sum(axis=0))
-    P = (X * sp.spdiags(1./gf, diags=0, m=n_features, n=n_features))
-    D_lang = pairwise_distances(P, metric=distance_language)
+    P = normalize(X, axis=1, norm='l1') # get probability distribution
+    if metric == 'jensen-shannon':
+        D_lang = pairwise_distances(P, metric=distance_language)
+    else:
+        D_lang = pairwise_distances(P, metric=metric)
     return D_lang
 
 
